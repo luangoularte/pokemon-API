7f8b9c97ddabee9866c60dff168cd1b009c31b5e
luangoularte <luan.ag2012@hotmail.com>
2024-01-22 12:00:58 -0300

    att

--- index.php
+++ index.php
@@ -2,11 +2,17 @@
 
 $cachePokemon = "pokemons.txt";
 
-$pagina = 1;
+$pagina = $_GET["pagina"];
 
-$url = "https://pokeapi.co/api/v2/pokemon?limit=150";
+$limite = 15;
+
+$inicio = ($pagina * $limite) - $limite;
+
+
+$url = "https://pokeapi.co/api/v2/pokemon?limit=150&offset=$inicio";
 $pokemons = json_decode(file_get_contents($url));
 
+file_put_contents($cachePokemon, var_export($pokemons, true));
 
 
 
@@ -23,21 +29,48 @@ $pokemons = json_decode(file_get_contents($url));
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    
 </head>
+
 <body>
     <h1>Lista Pokemons:</h1>
     <h2>
-        <ol>
-            <?php  foreach($pokemons->results as $dados){
+        <ol start=<?=$inicio+1?>>
+            <?php  
             
+            $array1 = $pokemons->results;
+            
+            $array1 = array_slice($array1, $inicio, $limite);
+    
+            foreach($array1 as $dados){
+                $nome = ucfirst($dados->name);
                     echo "
-                        <li>$dados->name</li>
+                        <li>$nome</li>
                     \n";
-            
                 }
             ?>
         </ol>
     </h2>
+    
+    
+    <div class="paginacao">
+        
+        <a href="?pagina=1">Início</a>
+
+        <?php if($pagina>1): ?>
+            <a href="?pagina=<?=$pagina-1?>"><<</a>
+        <?php endif ?>
+
+        <?=$pagina?>
+
+        <?php if($pagina<10): ?>
+            <a href="?pagina=<?=$pagina+1?>">>></a>
+        <?php endif ?>
+
+        <a href="?pagina=10">Fim</a>
+
+        
+
+    </div>
+
 </body>
 </html>
